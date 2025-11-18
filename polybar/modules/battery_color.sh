959c5d3999c27cb24ea10110fb4fa9ce3eaf0ddd
lepaks <90406444+lepaks@users.noreply.github.com>
2025-11-18 16:50:09 +0100

    Add files via upload

--- polybar/modules/battery_color.sh
+++ polybar/modules/battery_color.sh
@@ -4,9 +4,7 @@ percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$percentage" -ge 100 ]; then
     echo "%{F#00FF00}$percentage%"
-elif [ "$percentage" -ge 50 ]; then
-    echo "%{F#424153}$percentage%"
 else
-    echo "%{F#FF3333}$percentage%"
+    echo "$percentage"
 fi
 
